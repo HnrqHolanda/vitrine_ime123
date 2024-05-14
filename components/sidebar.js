763db66c94decc835c6458458c10aaeb5972dd97
HenriqHolanda <129341211+HenriqHolanda@users.noreply.github.com>
2024-05-14 19:43:26 -0300

    .

--- components/sidebar.js
+++ components/sidebar.js
@@ -4,15 +4,7 @@ import styles from "../styles/components/sidebar.module.css"
 const SideBarC= () =>{
     return(
         <div className={styles.container}>
-            <Link>Professores SE1</Link>
-            <Link>Professores SE2</Link>
-            <Link>Professores SE3</Link>
-            <Link>Professores SE4</Link>
-            <Link>Professores SE5</Link>
-            <Link>Professores SE6</Link>
-            <Link>Professores SE7</Link>
-            <Link>Professores SE8</Link>
-            <Link>Professores SE9</Link>
+
         </div>
     )
 }
